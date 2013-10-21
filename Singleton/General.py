7e6412feef621ae707d1a28355cce16977f87ef9
z2care <z2care@gmail.com>
2013-10-21 12:45:31 +0800

    add singleton design patterns.

---
+++ Singleton/General.py
@@ -0,0 +1,21 @@
+'''
+Created on 2013-10-19
+
+@author: zhangzhi
+@contact: z2care@gmail.com
+'''
+
+class Logger(object):
+    
+    def __new__(cls, *args, **kw):
+        if not hasattr(cls, '_instance'):
+            orig = super(Logger, cls)
+            cls._instance = orig.__new__(cls, *args, **kw)
+        return cls._instance
+
+if __name__ == '__main__':
+    a = Logger()
+    b = Logger()
+    print a
+    print b
+    assert id(a)==id(b)
